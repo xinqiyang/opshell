e97147bfca778ba223445967a7a3e6b3e1bdbc8b
xinqiyang <xinqiyang@gmail.com>
2013-04-09 18:21:04 +0900

    update file_cp sh

--- hadoop/file_cp.sh
+++ hadoop/file_cp.sh
@@ -10,12 +10,20 @@
 #   exit
 #fi
 
-
-
-iplist=`cat machine.conf`
+# type , $1 { scp, ssh },  src $2, dst $3
+user=hadoop
+iplist=`cat /home/hadoop/gitclone/opshell/hadoop/machine.conf`
 
 for ip in $iplist;do
-   scp -r $1 root@$ip:$2
+   echo "-----ops $ip -------------"
+   if [ "$1" == "scp" ] ; then	
+   	scp -r $2 $user@$ip:$3
+   elif [ "$1" == "ssh" ] ; then
+	cat ~/.ssh/id_rsa.pub | ssh $user@$ip "cat >> /home/hadoop/.ssh/authorized_keys"
+	ssh $user@$ip "chmod 700 /home/hadoop/.ssh/authorized_keys"
+   elif [ "$1" == "cmd" ] ; then
+	ssh $user@$ip "$2"
+   fi
 done
 
 

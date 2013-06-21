1c3bdd0e0ef930547d84b81468826b60fb6947fe
xinqiyang <xinqiyang@gmail.com>
2013-06-21 15:56:39 +0900

    add mac os x scripts

---
+++ mac/brew.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
+brew install wget
+
+
---
+++ mac/phpunit.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo pear channel-discover pear.phpunit.de
+sudo pear channel-discover components.ez.no
+sudo pear channel-discover pear.symfony-project.com
+sudo pear channel-discover pear.symfony.com
+sudo pear install phpunit/PHPUnit
+
+
+
